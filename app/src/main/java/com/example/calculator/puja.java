2b91284c13815ae3c3522697e1827c2b3d700339
Roy1707018 <roy1707018@stud.kuet.ac.bd>
2021-11-17 01:45:09 +0600

    java class added

---
+++ app/src/main/java/com/example/calculator/puja.java
@@ -0,0 +1,18 @@
+package com.example.calculator;
+
+public class puja {
+
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public puja(int a) {
+        this.a = a;
+    }
+}
